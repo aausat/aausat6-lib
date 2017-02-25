7053e694c1ec36c4a734f65c3639f7ee9c23988c
Anders Kalør <anders@kaloer.com>
2017-02-25 19:30:32 +0100

    Improve indentation

--- libfreertos/include/freertos/FreeRTOSConfig.h
+++ libfreertos/include/freertos/FreeRTOSConfig.h
@@ -91,36 +91,36 @@
 #include <avr/pgmspace.h>
 #define printf(s, ...) printf_P(PSTR(s), ## __VA_ARGS__)
 
-#define configUSE_PREEMPTION		1
+#define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK			1
 #define configUSE_TICK_HOOK			0
 
 extern unsigned long fcpu;
 //#define configCPU_CLOCK_HZ			( ( unsigned long ) 8000000 )
 #define configTICK_RATE_HZ			( ( TickType_t ) 100 )
-#define configMAX_PRIORITIES		( 4 )
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 200 )
-//#define configTOTAL_HEAP_SIZE		( (size_t ) ( 200 ) )
-#define configMAX_TASK_NAME_LEN		( 8 )
-#define configUSE_TRACE_FACILITY	1
-#define configUSE_16_BIT_TICKS		1
-#define configIDLE_SHOULD_YIELD		0
-#define configQUEUE_REGISTRY_SIZE	0
+#define configMAX_PRIORITIES			( 4 )
+#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 200 )
+//#define configTOTAL_HEAP_SIZE			( (size_t ) ( 200 ) )
+#define configMAX_TASK_NAME_LEN			( 8 )
+#define configUSE_TRACE_FACILITY		1
+#define configUSE_16_BIT_TICKS			1
+#define configIDLE_SHOULD_YIELD			0
+#define configQUEUE_REGISTRY_SIZE		0
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 		0
-#define configMAX_CO_ROUTINE_PRIORITIES ( 0 )
+#define configUSE_CO_ROUTINES 			0
+#define configMAX_CO_ROUTINE_PRIORITIES 	( 0 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
 #define INCLUDE_vTaskPrioritySet		0
 #define INCLUDE_uxTaskPriorityGet		0
-#define INCLUDE_vTaskDelete				1
-#define INCLUDE_vTaskCleanUpResources	0
+#define INCLUDE_vTaskDelete			1
+#define INCLUDE_vTaskCleanUpResources		0
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
-#define INCLUDE_vTaskDelay				1
+#define INCLUDE_vTaskDelay			1
 
 
 #endif /* FREERTOS_CONFIG_H */
